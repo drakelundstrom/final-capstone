6d975c0fe7f15e7c35b41c27aad806dac237556d
Jesse Hofmann <jessehofmann@yahoo.com>
2021-04-09 12:43:20 -0400

    pulling

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -4,11 +4,16 @@
     <h1>View your tournaments here</h1>
     <p>You must be authenticated to see this</p>
     <!-- <p> perhaps display past and current tournaments for current user?  </p> -->
+
   </div>
 </template>
 
 <script>
+
 export default {
-  name: "home"
+  name: "home",
+  components: {
+
+  }
 };
 </script>
